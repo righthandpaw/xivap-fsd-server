330c022d5667c946a5b2be4b7fd4bdb9dab78aa7
TOMATOCHAN\Robert_Oates <rwoates@gmail.com>
2018-09-14 09:01:11 +0900

    Added locks to the P2PHandshake registry

--- fsdp2ppool.py
+++ fsdp2ppool.py
@@ -1,32 +1,49 @@
 import re
-from fsdprotocol import fsdprotocol
+import threading
+import copy
 from threading import Lock, Thread
+from fsdprotocol import fsdprotocol 
 
-class fsdp2ppool:
+class fsdp2ppool():
 	
 	def __init__(self):
-		FSD					=	fsdprotocol()
+		self.FSD			= fsdprotocol()
 		self.__counter		= 0
 		self.__lock 		= Lock()
 		self.__clientPool 	= {}
 		
-	def AddRequests(self,message):
+	def AddRequests(self,words):
 		#$CQAAAA:BBBB:P2P:2:PPOS1:127.113.78.203:17504:192.168.0.7:17504
-		self.__lock.acquire()
+		self.__lock.acquire(True,1)
+		matches = re.match('\\'+self.FSD.FSDInfoRequest()+'([A-Za-z0-9]+)',words[0])
+		callsign = matches.group(1)
+	
 		self.__clientPool[self.__counter]={
-			"from":message[0],
-			"to":message[1],
-			"mode":message[3], 
-			"publicip":message[5], 
-			"publicport":message[6], 
-			"privateip":message[7],
-			"privateport": message[8]
+			"from":callsign,
+			"to":words[1],
+			"mode":words[3], 
+			"publicip":words[5], 
+			"publicport":words[6], 
+			"privateip":words[7],
+			"privateport": words[8]
 		}
 		self.__counter = self.__counter+1
-		lock.release()
+		self.__lock.release()
 		
 	def GetRequests(self,callsign):
-		print(self.__clientPool)
-		
+	
+		self.__lock.acquire(True,1)
+		#take a snapshot of the clientpool since the size could be modified
+		x = 0
+		localPool = self.__clientPool.copy()
+		localRegistry = {}
+
+		for i in localPool:
+			if callsign in localPool[i]["to"]:
+				localRegistry[x] = localPool[i]
+				x=x+1
+
+		self.__lock.release()
+		return localRegistry
 		
 			
