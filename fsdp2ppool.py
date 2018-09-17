47e5fb0d23aae7e6cafcab8a36354852b82df058
TOMATOCHAN\Robert_Oates <rwoates@gmail.com>
2018-09-18 02:48:00 +0900

    P2Phandshake class correctly exchanges data through a dict data object
    Still need to resolve issue of que syncing. Client 1 has to wait till Client 2 has finished filling its que before Client 1 can read it, otherwise a partial handshake takes place. Need to find a way to poll the cue for data.

--- fsdp2ppool.py
+++ fsdp2ppool.py
@@ -1,49 +1,47 @@
 import re
 import threading
 import copy
-from threading import Lock, Thread
-from fsdprotocol import fsdprotocol 
+from fsdprotocol import fsdprotocol
+from pprint import pprint  
 
 class fsdp2ppool():
 	
 	def __init__(self):
 		self.FSD			= fsdprotocol()
 		self.__counter		= 0
-		self.__lock 		= Lock()
 		self.__clientPool 	= {}
 		
 	def AddRequests(self,words):
 		#$CQAAAA:BBBB:P2P:2:PPOS1:127.113.78.203:17504:192.168.0.7:17504
-		self.__lock.acquire(True,1)
+		
 		matches = re.match('\\'+self.FSD.FSDInfoRequest()+'([A-Za-z0-9]+)',words[0])
-		callsign = matches.group(1)
-	
-		self.__clientPool[self.__counter]={
-			"from":callsign,
-			"to":words[1],
-			"mode":words[3], 
-			"publicip":words[5], 
-			"publicport":words[6], 
-			"privateip":words[7],
-			"privateport": words[8]
+		fromCallsign 	= matches.group(1)
+		toCallsign 		= words[1]
+		mode 			= words[3]
+		publicip 		= words[5]
+		publicport 		= words[6]
+		privateip 		= words[7]
+		privateport 	= words[8]
+		key = ("{}{}").format(fromCallsign,toCallsign)
+		
+		self.__clientPool[key]={
+			
+			'fromCallsign':fromCallsign,
+			'mode':mode,
+			'publicip':publicip,
+			'publicport':publicport,
+			'privateip':privateip,
+			'privateport':privateport,
+			'toCallsign':toCallsign,
 		}
-		self.__counter = self.__counter+1
-		self.__lock.release()
-		
-	def GetRequests(self,callsign):
-	
-		self.__lock.acquire(True,1)
-		#take a snapshot of the clientpool since the size could be modified
-		x = 0
-		localPool = self.__clientPool.copy()
-		localRegistry = {}
 
-		for i in localPool:
-			if callsign in localPool[i]["to"]:
-				localRegistry[x] = localPool[i]
-				x=x+1
-
-		self.__lock.release()
-		return localRegistry
 		
+	def GetRequests(self,toCallsign):
+		
+		local = {}
+		for key in self.__clientPool.keys():
+			if toCallsign in self.__clientPool[key]['toCallsign']:
+				local[key]= self.__clientPool[key]
+		return local	
+			
 			
