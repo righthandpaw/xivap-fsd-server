d2661d64fe8701951657d555437b2373ae26a241
TOMATOCHAN\Robert_Oates <rwoates@gmail.com>
2018-09-21 00:39:00 +0900

    Added a first-come-first serve priotiy que

--- fsdp2ppool.py
+++ fsdp2ppool.py
@@ -12,17 +12,14 @@ class fsdp2ppool():
 		self.__counter		= 0
 		self.__clientPool 	= {}
 		self.td 			= threading.Condition()
+		self.count			= 0
+		
 		
 	def AddRequests(self,words):
 		#$CQAAAA:BBBB:P2P:2:PPOS1:127.113.78.203:17504:192.168.0.7:17504
 		#$CRBBBB:AAAA:P2P:2:PPOS1:127.113.78.203:17504:192.168.0.7:17504
 		
-		
-		#self.lock.acquire(True)
-		
-		
 		status=self.td.acquire()
-		
 		
 		matches = re.match('(\\'+self.FSD.FSDInfoRequest()+'|\\'+self.FSD.FSDInfoReply()+')([A-Za-z0-9]+)',words[0])
 		requesttype		= matches.group(1)
@@ -33,26 +30,46 @@ class fsdp2ppool():
 		publicport 		= words[6]
 		privateip 		= words[7]
 		privateport 	= words[8]
-		key = ("{}{}").format(fromCallsign,toCallsign)
+		key 			= ("{}{}").format(fromCallsign,toCallsign)
+		reversekey 		= ("{}{}").format(toCallsign,fromCallsign)
+		self.count 		= self.count+1
 		
-		self.__clientPool[key]={
-			'requesttype':requesttype,
-			'fromCallsign':fromCallsign,
-			'mode':mode,
-			'publicip':publicip,
-			'publicport':publicport,
-			'privateip':privateip,
-			'privateport':privateport,
-			'toCallsign':toCallsign,
-			'status':'pending'
-		}
+		doesExist		= False
+		
+		#check if the reverse key exists already
+		#if it does we need to check the Request
+		
+		print("Pass #",self.count)
+		print("does this key: {} exist?".format(reversekey))
+		if reversekey in self.__clientPool:
+			print("yes ... gotta check something")
+			if self.__clientPool[reversekey]['requesttype'] == requesttype:
+				print("We already sent this so we do not have to send again.")
+				doesExist = True
+			else:
+				print("This does not exist so we can add it to the pile")
+				doesExist = False
+			
+		else:
+			doesExist = False
+			print("no ... adding! ",key)
+			
+		if doesExist == False:	
+			self.__clientPool[key]={
+				'requesttype':requesttype,
+				'fromCallsign':fromCallsign,
+				'mode':mode,
+				'publicip':publicip,
+				'publicport':publicport,
+				'privateip':privateip,
+				'privateport':privateport,
+				'toCallsign':toCallsign,
+				'status':'pending'
+			}
 		
 		status = self.td.release()
 		
-		
-		
-		
-		
+
 	def GetRequests(self,toCallsign):
 
 		clientPool = self.__clientPool.copy()
--- library-test.py
+++ library-test.py
@@ -18,47 +18,73 @@ class tester():
 		
 		
 		a_thread = threading.Thread(target=self.workerA,)
-		
-
 		b_thread = threading.Thread(target=self.workerB,)
-		b_thread.start()
+		c_thread = threading.Thread(target=self.workerB,)
 		a_thread.start()
+		b_thread.start()
+		c_thread.start()
+		
 		
 	def workerA(self):	
 
 		
-	
-	
+		w_thread = threading.Thread(target=self.watcher,args=('NR1918',))
+		w_thread.start()		
+
 		sentences = [
 					'$CQNR1918:NR1919:P2P:2:PPOS1:114.158.182.21:24062:127.0.0.1:29587',
+					'$CQNR1918:NR1920:P2P:2:PPOS1:114.158.182.21:24062:127.0.0.1:29587',
+					'$CRNR1918:NR1919:P2P:2:PPOS1:114.158.182.21:24062:127.0.0.1:29587',
+					'$CRNR1918:NR1920:P2P:2:PPOS1:114.158.182.21:24062:127.0.0.1:29587',
 					]
 		
 		for sentence in sentences:
 			self.FSDp2ppool.AddRequests(sentence.split(':'))
 		
-		w_thread = threading.Thread(target=self.watcher,args=('NR1918',))
-		w_thread.start()		
-
 
 	def workerB(self):
 		
 
-		sentences = [
-					'$CQNR1919:NR1918:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
-
-					]
 		
-		for sentence in sentences:
-			self.FSDp2ppool.AddRequests(sentence.split(':'))
 	
 		watcher = threading.Thread(target=self.watcher,args=('NR1919',))
 		watcher.start()
 
 		
+		sentences = [
+					'$CQNR1919:NR1918:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					'$CQNR1919:NR1920:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					'$CRNR1919:NR1918:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					'$CRNR1919:NR1920:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					]
+		
+		for sentence in sentences:
+			self.FSDp2ppool.AddRequests(sentence.split(':'))
+	
+
+
+	def workerC(self):
+		
+		watcher = threading.Thread(target=self.watcher,args=('NR1920',))
+		watcher.start()
+
+		
+		sentences = [
+					'$CQNR1920:NR1918:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					'$CQNR1920:NR1919:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					'$CRNR1920:NR1918:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512',
+					'$CRNR1920:NR1919:P2P:2:PPOS1:114.158.182.21:21870:127.0.0.1:29512'
+					]
+		
+		for sentence in sentences:
+			self.FSDp2ppool.AddRequests(sentence.split(':'))
+
+
+	
+		
 	def watcher(self,callsign):
 	
-		print("Give me all messages for Callsign - {}".format(callsign))
-		#run this in a loop 
+		
 		
 		while True:
 			waiting = True
@@ -69,17 +95,14 @@ class tester():
 			p2pclient = self.FSDp2ppool.GetRequests(callsign)
 			for key in p2pclient:
 				if p2pclient[key]['status'] == 'pending':
-					#print("sending")
-					#print(p2pclient[key])
+					print("Give me all messages for Callsign - {}".format(callsign))
+					print("sending")
+					print(p2pclient[key])
 					self.FSDp2ppool.UpdateRequests(key)
 					print(p2pclient[key])
 		
 		
-		
-	
-		
-
-		
+				
 	
 tester().main()	
 #MyServer = tester()
